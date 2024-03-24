575f8a2859cf25256690340eefc620c4122fc8a6
FeltenA <56635725+FeltenA@users.noreply.github.com>
2024-03-24 20:00:29 +0100

    Update script.sh

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -4,6 +4,3 @@ sleep 10
 wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
-
-mkdir -p /run/php
-/usr/sbin/php-fpm7.3 -F
