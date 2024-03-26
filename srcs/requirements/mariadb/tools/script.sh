d99d63de1b188106347486e066fb1e0234433e93
FeltenA <56635725+FeltenA@users.noreply.github.com>
2024-03-26 11:08:49 +0100

    Update script.sh

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,10 +2,10 @@
 
 service mysql start;
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > database_setup.sql
-echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> database_setup.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> database_setup.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" >> database_setup.sql
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > database_setup.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> database_setup.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';" >> database_setup.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> database_setup.sql
 echo "FLUSH PRIVILEGES;" >> database_setup.sql
 mysql < database_setup.sql
 
