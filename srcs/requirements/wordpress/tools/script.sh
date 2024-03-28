6400a78c64d9d4249e808d369ffe22165a73fdfc
FeltenA <56635725+FeltenA@users.noreply.github.com>
2024-03-28 15:52:26 +0100

    Update script.sh

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -10,3 +10,5 @@ sed -i -r "s/password_here/$MYSQL_PASSWORD/1"    /wp-config.php
 mv /wp-config.php /var/www/html
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+
+/usr/sbin/php-fpm7.3 -F
